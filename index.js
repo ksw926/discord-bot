ebcf579687aa8f94dee0a158334fc2cdf3d2eb40
ksw926 <k9s2w6@naver.com>
2026-08-25 14:57:47 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+app.get('/', (req, res) => res.send('Bot is running!'));
+app.listen(3000);
+
+const express = require('express');
+const app = express();
 app.get('/', (req, res) => res.send('거점봇 작동 중!'));
 app.listen(process.env.PORT || 3000);
 
